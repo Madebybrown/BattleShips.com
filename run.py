988dc7e201a817cfc1ae17a787398d2816320007
Madebybrown <madebybrown@gmail.com>
2022-10-08 09:30:37 +0000

    Update the board size

--- run.py
+++ run.py
@@ -1,26 +1,28 @@
 from random import randint
 
 # Creates the player and computers board sizes
-PLAYER_BOARD = [[" "] * 8 for x in range(8)]
-COMPUTER_BOARD = [[" "] * 8 for i in range(8)]
+PLAYER_BOARD = [[" "] * 5 for x in range(5)]
+COMPUTER_BOARD = [[" "] * 5 for i in range(5)]
 
 # Converts letters to numbers
 letter_to_number = {
     'A': 0, 'B': 1, 'C': 2, 'D': 3,
-    'E': 4, 'F': 5, 'G': 6, 'H': 7
+    'E': 4
 }
+
 
 def print_board(board):
     """
     Prints the player and computer board to the terminal
     """
-    print('  A B C D E F G H')
-    print('  ---------------')
+    print('  A B C D E')
+    print('  ---------')
     row_num = 1
 
     for row in board:
         print("%d|%s|" % (row_num, "|".join(row)))
         row_num += 1
+
 
 def spawn_ship(board):
     """
@@ -29,11 +31,11 @@ def spawn_ship(board):
     Otherwise place "X" on the board
     """
     for ship in range(5):
-        ship_row, ship_column = randint(0,7), randint(0,7)
-        
+        ship_row, ship_column = randint(0,4), randint(0,4)     
         while board[ship_row][ship_column] == "X":
-            ship_row, ship_column = randint(0,7), randint(0,7)
+            ship_row, ship_column = randint(0,4), randint(0,4)
         board[ship_row][ship_column] = "X"
+
 
 def ship_position():
     """
@@ -41,16 +43,17 @@ def ship_position():
     Checks if the input is valid
     Returns row as an integer and removes 1 for indexing, converts letters to numbers
     """
-    row = input("Please enter a ship row 1-8: \n")
-    while row not in '12345678':
+    row = input("Please enter a ship row 1-5: \n")
+    while row not in '12345':
         print("Please enter a valid row number: \n")
-        row = input("Please enter a ship row 1-8: \n")
+        row = input("Please enter a ship row 1-5: \n")
 
-    column = input("Please enter a ship column A-H: \n")
-    while column not in "ABCDEFGH":
+    column = input("Please enter a ship column A-E: \n")
+    while column not in "ABCDE":
         print("Please enter a valid ship column: \n")
-        column = input("Please enter a ship column A-H: \n").upper()
+        column = input("Please enter a ship column A-E: \n").upper()
     return int(row) - 1, letter_to_number[column]
+
 
 def hit_ships(board):
     """
@@ -63,6 +66,7 @@ def hit_ships(board):
             if column == "X":
                 count += 1
     return count
+
 
 # Spawns board, sets the turns to 10
 # Displays messages in relation to what input data is recieved from the player
@@ -89,5 +93,5 @@ def main():
                 break
             print("You have " + str(turns) + " turns remaining")
             if turns == 0:
-                print("You ran out of turns, Game Over!")
+                print("You ran out of turns, Game Over!")            
 main()
